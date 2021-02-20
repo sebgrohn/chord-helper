716c8f0a04ffae03b3ec90373737bc83b4fc2aa0
Sebastian Fehrnström-Gröhn <sebastian@greenbyte.com>
2021-02-20 16:42:19 +0100

    Empty App

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,14 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import './App.css';
-import logo from './logo.svg';
 import { theme } from './theme';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header"></header>
+      </div>
     </ThemeProvider>
   );
 }
