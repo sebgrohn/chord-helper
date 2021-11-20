cf28215b3a541996f4740c9b1b0be89e81ced2cf
Sebastian Fehrnström-Gröhn <sebastian@greenbyte.com>
2021-11-20 21:45:43 +0100

    Add tests for transposeChord

--- src/Theory/chords.spec.ts
+++ src/Theory/chords.spec.ts
@@ -1,8 +1,15 @@
-import { getChordNotes } from './chords';
+import { getChordNotes, transposeChord } from './chords';
 
 describe('getChordNotes', () => {
   it('should return C, E, G for C major chord', () => {
     const noteNames = getChordNotes('C');
     expect(noteNames).toEqual(['C', 'E', 'G']);
+  });
+});
+
+describe('transposeChord', () => {
+  it('should return E major chord for C major transposed a perfect forth', () => {
+    const transposedChord = transposeChord('C', 5);
+    expect(transposedChord).toEqual('F');
   });
 });
